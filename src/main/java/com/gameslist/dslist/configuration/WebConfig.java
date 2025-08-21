44e5a1a79987dc3becd72715b67b5d27076fa46a
Rodrigo <rodrigudesousa@gmail.com>
2025-08-21 09:58:11 -0400

    CORS allow methods

--- src/main/java/com/gameslist/dslist/configuration/WebConfig.java
+++ src/main/java/com/gameslist/dslist/configuration/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("*").allowedOrigins(corsOrigins);
+                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedOrigins(corsOrigins);
             }
         };
     }
